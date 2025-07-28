ed83891147ebe6ac55f32f544f04725bd07aafef
Jamal Williams <jwill702@charlotte.edu>
2025-07-27 21:59:58 -0400

    feat: added delete endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Response
+
 from models.db import db
 from models.models import Sheep
 
@@ -6,6 +7,8 @@ app = FastAPI()
 
 @app.get("/sheep/{id}", response_model=Sheep)
 def read_sheep(id: int):
+    if id not in db.data:
+        raise HTTPException(status_code=404, detail="Sheep not found")
     return db.get_sheep(id)
 
 
@@ -26,3 +29,10 @@ def update_sheep(sheep: Sheep):
 
     db.data[sheep.id] = sheep
     return sheep
+
+@app.delete("/sheep/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_sheep(id: int):
+    if id not in db.data:
+        raise HTTPException(status_code=404, detail="Sheep with this ID doesn't exist")
+    del db.data[id]
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
